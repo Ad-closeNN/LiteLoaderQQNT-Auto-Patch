4bf1361d4e14624e412305bde42e96c82d4b0318
youzi-2333 <youzi5201086@163.com>
2024-08-10 10:35:27 +0800

    :art: 增强代码可读性

--- main.py
+++ main.py
@@ -2,37 +2,31 @@
 程序入口点。
 """
 
+from pathlib import Path
+import sys
 import tkinter as tk
 from tkinter import filedialog
 import logging
 import os
 import datetime
-import time
 
 # 弄获取 %localappdata% ({system_disk}:\Users\{username}\Appdata\local)的路径
-localappdata = os.path.expandvars("%LOCALAPPDATA%")
+LOCALAPPDATA = Path(os.path.expandvars("%LOCALAPPDATA%"))
 # 配置日志记录
-if not os.path.exists(f"{localappdata}/LiteLoaderQQNT Auto Patch"):
-    os.mkdir(f"{localappdata}/LiteLoaderQQNT Auto Patch/")
-    os.mkdir(f"{localappdata}/LiteLoaderQQNT Auto Patch/Logs")
-if not os.path.exists(f"{localappdata}/LiteLoaderQQNT Auto Patch/Logs"):
-    os.mkdir(f"{localappdata}/LiteLoaderQQNT Auto Patch/Logs")
+(WORKDIR := LOCALAPPDATA / "LiteLoaderQQNT Auto Patch").mkdir(exist_ok=True)
+(LOGDIR := LOCALAPPDATA / "LiteLoaderQQNT Auto Patch/Logs").mkdir(exist_ok=True)
 
 
 def mklogs():
     """
     日志初始化。
     """
-    nowtime = datetime.datetime.now()
-    day_string = nowtime.strftime("%Y-%m-%d")
+    nowtime = datetime.datetime.now().strftime("%Y-%m-%d")
     logging.basicConfig(
-        filename=f"{localappdata}/LiteLoaderQQNT Auto Patch/Logs/{day_string}.log",
+        filename=LOGDIR / f"{nowtime}.log",
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
     )
-
-
-mklogs()
 
 
 # 检测有没有源目录
@@ -40,95 +34,71 @@ def vape_qq():
     """
     获取 QQNT 目录。如果找不到，弹窗要求用户选择 QQNT 源目录。
     """
-    if not os.path.exists(
-        f"{localappdata}/LiteLoaderQQNT Auto Patch/QQNT_file_path.ini"
-    ):
-        with open(
-            f"{localappdata}/LiteLoaderQQNT Auto Patch/QQNT_file_path.ini",
-            "w",
-            encoding="utf-8",
-        ) as f:
-            f.write("")
-        # 创建一个隐藏的主窗口
-        root = tk.Tk()
-        root.iconbitmap(default="C:/Windows/System32/Shell32.dll")
-        root.withdraw()  # 隐藏主窗口
-        # 打开文件选择对话框
-        qqnt_path = filedialog.askdirectory(title="选择 QQNT 的所在文件夹")
-        # 将选择的路径写入配置文件
-        if qqnt_path:
-            logging.info("QQNT 的文件路径: %s", qqnt_path)  # 打印文件路径
-            with open(
-                f"{localappdata}/LiteLoaderQQNT Auto Patch/QQNT_file_path.ini",
-                "w",
-                encoding="utf8",
-            ) as f:
-                f.write(qqnt_path)
-        else:
-            print("你没有选择 QQNT 的位置，请重试。")
+    if (ini := WORKDIR / "QQNT_file_path.ini").exists():
+        with ini.open("r", encoding="utf-8") as f:
+            return Path(f.read())
+    # 创建一个隐藏的主窗口
+    root = tk.Tk()
+    root.iconbitmap(default="C:/Windows/System32/Shell32.dll")
+    root.withdraw()  # 隐藏主窗口
+    # 打开文件选择对话框
+    qqnt_path = filedialog.askdirectory(title="选择 QQNT 的所在文件夹")
+    # 将选择的路径写入配置文件
+    if not qqnt_path:
+        print("你没有选择 QQNT 的位置，请重试。")
+        return None
+    logging.info("QQNT 的文件路径: %s", qqnt_path)  # 打印文件路径
+    qqnt_path = Path(qqnt_path)
+    if not qqnt_path.exists():
+        print("你选择的 QQNT 的位置不存在，请重试。")
+        return None
 
-            time.sleep(3)
+    with ini.open("w", encoding="utf-8") as f:
+        f.write(qqnt_path.as_posix())
+
+    return qqnt_path
 
 
 def vape_ll():
     """
     获取 LiteLoaderQQNT 目录。如果找不到，弹窗要求用户选择。
     """
-    if not os.path.exists(
-        f"{localappdata}/LiteLoaderQQNT Auto Patch/LiteLoaderQQNT_file_path.ini"
-    ):
-        with open(
-            f"{localappdata}/LiteLoaderQQNT Auto Patch/LiteLoaderQQNT_file_path.ini",
-            "w",
-            encoding="utf-8",
-        ) as llqqnt:
-            llqqnt.write("")
-        # 创建一个隐藏的主窗口
-        root = tk.Tk()
-        root.iconbitmap(default="C:/Windows/System32/Shell32.dll")
-        root.withdraw()  # 隐藏主窗口
-        # 打开文件选择对话框
-        llqqnt_path = filedialog.askdirectory(title="选择 LiteLoaderQQNT 的所在文件夹")
-        # 将选择的路径写入配置文件
-        if llqqnt_path:
-            logging.info("LiteLoaderQQNT 的文件路径: %s", llqqnt_path)  # 打印文件路径
-            with open(
-                f"{localappdata}/LiteLoaderQQNT Auto Patch/LiteLoaderQQNT_file_path.ini",
-                "w",
-                encoding="utf8",
-            ) as llqqnt:
-                llqqnt.write(llqqnt_path)
-        else:
-            print("你没有选择 LiteLoaderQQNT 的位置，请重试。")
+    if (ini := WORKDIR / "LiteLoaderQQNT_file_path.ini").exists():
+        with ini.open("r", encoding="utf-8") as f:
+            return Path(f.read())
+    # 创建一个隐藏的主窗口
+    root = tk.Tk()
+    root.iconbitmap(default="C:/Windows/System32/Shell32.dll")
+    root.withdraw()  # 隐藏主窗口
+    # 打开文件选择对话框
+    qqnt_path = filedialog.askdirectory(title="选择 LiteLoaderQQNT 的所在文件夹")
+    # 将选择的路径写入配置文件
+    if not qqnt_path:
+        print("你没有选择 LiteLoaderQQNT 的位置，请重试。")
+        return None
+    logging.info("LiteLoaderQQNT 的文件路径: %s", qqnt_path)  # 打印文件路径
+    qqnt_path = Path(qqnt_path)
+    if not qqnt_path.exists():
+        print("你选择的 LiteLoaderQQNT 的位置不存在，请重试。")
+        return None
 
-            time.sleep(3)
+    with ini.open("w", encoding="utf-8") as f:
+        f.write(qqnt_path.as_posix())
+
+    return qqnt_path
 
 
-vape_qq()
-vape_ll()
-if os.path.exists(f"{localappdata}/LiteLoaderQQNT Auto Patch/QQNT_file_path.ini"):
-    if os.path.exists(
-        f"{localappdata}/LiteLoaderQQNT Auto Patch/LiteLoaderQQNT_file_path.ini"
-    ):
-        with open(
-            f"{localappdata}/LiteLoaderQQNT Auto Patch/LiteLoaderQQNT_file_path.ini",
-            "r",
-            encoding="utf-8",
-        ) as latest_loc:
-            qqnt_latest_location = latest_loc.read()
-        with open(
-            f"{localappdata}/LiteLoaderQQNT Auto Patch/QQNT_file_path.ini",
-            "r",
-            encoding="utf-8",
-        ) as qqnt:
-            vape = qqnt.read()
-            with open(
-                f"{vape}/resources/app/app_launcher/index.js", "w", encoding="utf-8"
-            ) as fxxkqq:
-                fxxkqq.write(
-                    f"require(String.raw`{qqnt_latest_location}`);\nrequire('./launcher.node').load('external_index', module);"
-                )
-    else:
-        print("error")
-else:
-    print("error")
+if __name__ == "__main__":
+    mklogs()
+    PATHQQ = vape_qq()
+    if not PATHQQ:
+        sys.exit(-1)
+    PATHLL = vape_ll()
+    if not PATHLL:
+        sys.exit(-1)
+    with (PATHQQ / "resources/app/app_launcher/index.js").open(
+        "w", encoding="utf-8"
+    ) as fxxkqq:
+        fxxkqq.write(
+            f"require(String.raw`{PATHLL}`);\nrequire('./launcher.node').load('external_index', module);"
+        )
